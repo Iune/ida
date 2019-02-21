da631028df3393bdb4c2822ee9cc97fef1c3020f
Aditya Duri <aditya.duri@gmail.com>
2019-02-20 19:39:47 -0600

    Added loop to parse multiple voters

--- main.go
+++ main.go
@@ -1,7 +1,12 @@
 package main
 
 import (
+	"bufio"
+	"fmt"
 	"os"
+	"strings"
+
+	"github.com/atotto/clipboard"
 
 	"github.com/iune/ida/contest"
 	"github.com/iune/ida/results"
@@ -17,16 +22,47 @@ func main() {
 	countries := contest.LoadCountries("countries.tsv")
 	contest := contest.LoadContest("1991.csv", countries)
 
-	lines := []string{
-		":12: :yugoslavia: Yugoslavia: Bebi Doll - Brazil",
-		":10: :sweden: Sweden: Carola - Fångad av en stormvind",
-		":8: :france: France: Amina - C'est le dernier qui a parlé raison",
-		"Hello world",
-		":cyprus: Cyprus: Elena Patroklou - SOS",
-		":7: UK: Samantha Janus - Message to Your Heart",
-	}
+	repeat := true
+	for repeat {
+		// Get voter name
+		fmt.Print("Country Name:\n> ")
+		input := bufio.NewScanner(os.Stdin)
+		voterName := input.Text()
+		if err := input.Err(); err != nil {
+			log.Fatal(err)
+		}
 
-	votes := voting.Find(contest, lines)
-	results.Output(contest, votes, "Yugoslavia")
-	results.PrintVotes(votes)
+		// Read in votes from standard input
+		var lines []string
+		input = bufio.NewScanner(os.Stdin)
+		for input.Scan() {
+			lines = append(lines, input.Text())
+		}
+		if err := input.Err(); err != nil {
+			log.Fatal(err)
+		}
+		fmt.Println()
+
+		// Parse and print votes to standard output and to clipboard
+		votes := voting.Find(contest, lines)
+		outputString := results.Output(contest, votes, voterName)
+		clipboard.WriteAll(outputString)
+
+		results.PrintVotes(votes)
+		fmt.Println()
+
+		// Check to see if we should continue the loop
+		fmt.Print("Continue? (Y/N)\n> ")
+		input = bufio.NewScanner(os.Stdin)
+		input.Scan()
+		if err := input.Err(); err != nil {
+			log.Fatal(err)
+		}
+
+		text := input.Text()
+		if strings.ToLower(text) == "n" {
+			repeat = false
+		}
+		fmt.Println()
+	}
 }
