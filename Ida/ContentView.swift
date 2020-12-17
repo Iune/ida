854b37d905a93bde7857a997692177dbd2ef86b7
Aditya Duri <aditya.duri@gmail.com>
2020-12-17 12:42:20 -0500

    Initial Commit

---
+++ Ida/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Ida
+//
+//  Created by Aditya Duri on 12/17/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Ida/IdaApp.swift
@@ -0,0 +1,17 @@
+//
+//  IdaApp.swift
+//  Ida
+//
+//  Created by Aditya Duri on 12/17/20.
+//
+
+import SwiftUI
+
+@main
+struct IdaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
