3bdde528b962ef9f511b4ea5058e33adee8c56df
Aditya Duri <aditya.duri@gmail.com>
2021-07-03 02:06:08 -0500

    Updating to make npm link work

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
+#!/usr/bin/env node
+
 import { Command } from 'commander';
 import { spreadsheet } from "./commands";
 
@@ -6,7 +8,7 @@ const program = new Command();
 program
     .version('0.4.0')
     .command('spreadsheet <countriesFile> <entriesFile> <outputFilePrefix>')
-    .description('Generate Excel spreadsheet and contest JSON file based on input country and entry details')
+    .description('Generate Excel spreadsheet and contest JSON file based on country and entry details')
     .action(async (countriesFile, entriesFile, outputFilePrefix) => {
         spreadsheet(countriesFile, entriesFile, outputFilePrefix)
     });
